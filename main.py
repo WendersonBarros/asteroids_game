5ed893e63bf59bc19152bd55b39e762bf4060607
WendersonBarros <wendercesar2@gmail.com>
2024-10-05 23:03:08 -0400

    feat: the `Player` can shoot bullets

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from asteroid import Asteroid
 from asteroidfield import AsteroidField
 from constants import *
 from player import Player
+from shot import Shot
 
 def main():
     pygame.init()
@@ -12,11 +13,14 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
-    
+    shots = pygame.sprite.Group()
+
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = updatable
     asteroid_field = AsteroidField()
-    
+
+    Shot.containers = (shots, updatable, drawable)
+
     Player.containers = (updatable, drawable)
     player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
 
